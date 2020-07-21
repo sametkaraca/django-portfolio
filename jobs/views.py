8d5734dc8c6759d7e829961980b4fb36ec35dada
Sam <you@example.com>
2020-07-21 23:28:57 +0200

    Denemeee

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+#
+# # Create your views here.
+# I made some changes Create your views here.
 # Create your views here.
+# I made some changes# Create your views here.
+# I made some changes# Create your views here.
+# I made some changes# Create your views here.
+# I made some changes# Create your views here.
+# I made some changes# Create your views here.
+# I made some changes# Create your views here.
+# I made some changes
+# I made some changes
